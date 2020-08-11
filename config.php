935fe83aa0d50c05f3b6fe6beb1fd4e993e47f01
Karan <69091155+karanchandru@users.noreply.github.com>
2020-08-11 22:13:57 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost="localhost"; //hostname
+$dbuser="root";  //mysql acc/ username
+$dbpass="";  //mysql scc/ password
+$dbname="prof"; //mysql database name
+// $db = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+?>
